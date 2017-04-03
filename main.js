73489666cbc29cf39c84a963043f5019ce128613
Sandeep <sjnews@gmail.com>
2017-04-03 18:35:07 -0400

    tweaks and comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
-	$('#borough').change(function () {
-		var myFeelings = $('#borough').find(':selected');
-		feeling = myFeelings.data('feelings');
-		console.log("How I feel about that place: ", feeling);
+	$('#borough').on("change", function () { //note: change event
+		var myFeelings = $('#borough').find(':selected'); // variable myFeelings holds selected item
+		feeling = myFeelings.data('feelings'); // use data method to grab feelings info from html
+		console.log(feeling); //let's see if it works
 	});
 
 
